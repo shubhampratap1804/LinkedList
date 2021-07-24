fe6c7df2bde8a57940719b38d2ae14bc81d9a84a
pratap1804 <pratap.1804@gmail.com>
2021-07-25 00:00:04 +0530

    Created: a simple linked list of 56, 30 and 70

--- src/com/bridgelabz/MyNode.java
+++ src/com/bridgelabz/MyNode.java
@@ -1,8 +1,28 @@
 package com.bridgelabz;
 
-public class MyNode {
+public class MyNode<K> {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to linked list program!");
+	private K key;
+	private MyNode<K> next;
+
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public K getKey() {
+		return this.key;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+
+	public MyNode<K> getNext() {
+		return this.next;
 	}
 }
